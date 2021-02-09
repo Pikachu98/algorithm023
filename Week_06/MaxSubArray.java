a275927bae5f61f84a983a84eca41a859741c1ff
Qianwen Zhang <326200153@qq.com>
2021-02-09 02:50:23 +0000

    “week6_ver3”

--- Week_06/MaxSubArray.java
+++ Week_06/MaxSubArray.java
@@ -4,15 +4,19 @@ public class MaxSubArray {
     public static int maxSubArray(int[] nums) {
         int max = nums[0];
 //        int sum = nums[0];
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+//        int[] dp = new int[nums.length];
+//        dp[0] = nums[0];
+        int sum = nums[0];
 
         // 不停求和one by one，存到dp数组里，直到当前数字的值大于之前的和，停止
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
-            // 找到dp数组里的最大值
-            if (dp[i] > max)
-                max = dp[i];
+//            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            sum = Math.max(sum + nums[i], nums[i]);
+//          找到dp数组里的最大值
+            if (sum > max)
+                max = sum;
+//            if (dp[i] > max)
+//                max = dp[i];
 
 //            这么写不行，必须要数组存储前面相加出来的和
 //            sum = Math.max(sum + nums[i], sum);
